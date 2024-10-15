cd1fdc9671760f81fe1f149ed442767224f5e069
jaimezzapata <zapataval2304@gmail.com>
2024-10-15 10:28:07 -0500

    Implementación de función de registro

--- script.js
+++ script.js
@@ -5,17 +5,44 @@ let usuarios = [
         contrasena: '1234567',
         profesion: 'Developer',
         usuario: 'admin'
-    }
+    },
+    {
+        nombre: 'Mariana Reestrepo',
+        correo: 'correo@correo.com',
+        contrasena: '1234567',
+        profesion: 'UX/UI',
+        usuario: 'mariana'
+    },
 ]
 console.log(usuarios)
 function iniciarSesion() {
     let usuario = document.getElementById("usuario").value
     let contrasena = document.getElementById("contrasena").value
-    if (usuario == usuarios[0].usuario && contrasena == '1234') {
+    // let auth = usuarios.find((item) => usuario == item.usuario)
+    let auth = usuarios.some((item) => usuario == item.usuario)
+    console.log(auth)
+    // usuarios.some(()=>)
+    if (auth) {
         console.log('Inicio de sesión')
     } else {
         console.log('Error de credenciales')
     }
+}
+
+function registrarUsuario() {
+    let usuario = document.getElementById("rusuario").value
+    let contrasena = document.getElementById("rcontrasena").value
+    let correo = document.getElementById("rcorreo").value
+    let nombre = document.getElementById("rnombre").value
+    let profesion = document.getElementById("rprofesion").value
+    let objeto = {
+        usuario: usuario,
+        contrasena: contrasena,
+        correo: correo,
+        nombre: nombre,
+        profesion: profesion
+    }
+    usuarios.push(objeto)
 }
 
 let objeto = {
