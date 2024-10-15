36263dfad46c4053f64a1645c6bf4183f3346413
jaimezzapata <zapataval2304@gmail.com>
2024-10-15 08:20:38 -0500

    Captura de datos de los input por medio de la propiedad value

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function iniciarSesion(){
-    console.log('Inicio de sesión')
-    let label = document.getElementById('label')
-    console.log(label)
+function iniciarSesion() {
+    // console.log('Inicio de sesión')
+    // let label = document.getElementById('label')
+    // console.log(label)
+    let usuario = document.getElementById("usuario").value
+    let contrasena = document.getElementById("contrasena").value
+    if (usuario == 'Jaime' && contrasena == '1234') {
+        console.log('Inicio de sesión')
+    } else {
+        console.log('Error de credenciales')
+    }
 }
