55357ddef909afcf3968f65947174378839ad1d9
jaimezzapata <zapataval2304@gmail.com>
2024-11-12 08:59:08 -0500

    implementación de createElement para d efinir estructura de la card

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 import { recetasNoVegetarianas, recetasVegetarianas } from "./database.js";
 
 function crearCardReceta() {
-  let nombre = document.createElement("h3");
-  let descripcion = document.createElement("p");
-  let dificultad = document.createElement("p");
-  let tiempoPreparacion = document.createElement("p");
-  let tipo = document.createElement("p");
-  let ingredientes = document.createElement("p");
-  let listadoIngredientes = document.createElement("ul");
-  let ingrediente = document.createElement("li");
-  let pasos = document.createElement("p");
-  let listadoPasos = document.createElement("ol");
-  let paso = document.createElement("li");
-  let card = document.createElement("div");
-  listadoPasos.append(paso, paso, paso);
-  listadoIngredientes.append(ingrediente, ingrediente, ingrediente);
-  card.append(
-    nombre,
-    descripcion,
-    dificultad,
-    tiempoPreparacion,
-    tipo,
-    ingredientes,
-    listadoIngredientes,
-    pasos,
-    listadoPasos
-  );
-  console.log(card);
+  recetasVegetarianas.map(() => {
+    let nombre = document.createElement("h3");
+    let descripcion = document.createElement("p");
+    let dificultad = document.createElement("p");
+    let tiempoPreparacion = document.createElement("p");
+    let tipo = document.createElement("p");
+    let ingredientes = document.createElement("p");
+    let listadoIngredientes = document.createElement("ul");
+    let ingrediente = document.createElement("li");
+    let pasos = document.createElement("p");
+    let listadoPasos = document.createElement("ol");
+    let paso = document.createElement("li");
+    let card = document.createElement("div");
+    listadoPasos.append(paso, paso, paso);
+    listadoIngredientes.append(ingrediente, ingrediente, ingrediente);
+    card.append(
+      nombre,
+      descripcion,
+      dificultad,
+      tiempoPreparacion,
+      tipo,
+      ingredientes,
+      listadoIngredientes,
+      pasos,
+      listadoPasos
+    );
+    console.log(card);
+  });
 }
 
 function agregarRecetaHtml() {}
